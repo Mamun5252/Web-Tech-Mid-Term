f5a9b0d5e5b1165874c3deff1e7f44f33913b0ae
Abdullah Al Mamun <mamunkhan01718@gmail.com>
2022-01-31 21:34:22 +0600

    lab task 01

---
+++ Lab Task 01.php
@@ -0,0 +1,34 @@
+<html>
+    <body>
+<form>
+First Name:
+<input type="text" name="fname"><br>
+Last Name:
+<input type="text" name="fname">
+<br>
+Age:    
+<input type="text" name="fname">
+<br>
+Designation:
+<input type="radio">Junior Programmer
+<input type="radio">Senior Programmer
+<input type="radio">Project Lead
+<br>
+Preferred Language
+<input type="radio">JAVA
+<input type="radio">PHP
+<input type="radio">C++
+<br>
+E-mail:
+<input type="text" name="fname">
+<br>
+Password:
+<input type="text" name="fname">
+<br>
+Please choose a file <input type="submit" value="Choose File">No file chosen
+<br>
+<input type="Submit">
+<input type="Reset">
+</form>
+</body>
+</html>
